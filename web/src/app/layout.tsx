5ffdff8c5e9a6fb7e0618b195cc1a8cc748cdd12
Jonas Heller <jonasheller89@gmail.com>
2026-04-29 23:34:02 +0200

    fix: remove old light-theme.css that was overriding accent to purple

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata, Viewport } from "next";
 import "./globals.css";
-import "./light-theme.css";
 import { ServiceWorkerRegistration } from "./sw-register";
 import { ThemeProvider } from "@/components/ThemeProvider";
 
